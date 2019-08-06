895e8373c8d3e7fd3c109527af0656236cf95b11
Mathieu Kahlaoui <mathieukahlaoui@MacBook-Pro-de-Adminsqli.local>
2019-08-06 21:53:44 +0200

    slowmotion effect wip

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@
               xPercent      => the point of start displacement of the element about to its X origin axis.
               YPercent      => the point of start displacement of the element about to its Y origin axis.
               Ease          => the animation effect see https://greensock.com/ease-visualizer.
-              last argument => is the time between each animation
+              last argument => is the time between each animation is can be positive or negative number
         tips:
               .set([$projectTitle,$projectSubtitle,$pixel], {autoAlpha: 0}) => autoAlpha: 0 is mean set element invisible
     */
@@ -46,23 +46,24 @@
     */
     /*
         * add()
-            function add("label") is like a flag
+            function add("label") is like a
             https://greensock.com/docs/TimelineMax/add
     */
     tlProject
         .set([$projectTitle,$projectSubtitle,$pixel], {autoAlpha: 0}) // IMPORTANT: autoAlpha: 0 is mean set element invisible when the application start
         .fromTo($projectImage, 0.4,{autoAlpha: 0, xPercent: "-200"},{autoAlpha: 1, xPercent: "-10", ease: Power4.easeInOut})
         .add("imageIn") // add label just after the precedent animation is finish
-        .staggerFromTo($pixel, 0.3, {autoAlpha: 0, xPercent: "-=10"}, {autoAlpha: 1, x: "0", ease: Power4.easeInOut}, 0.02)
+        .staggerFromTo($pixel, 0.3, {autoAlpha: 0, xPercent: "-=10"}, {autoAlpha: 1, x: "0", ease: Power4.easeInOut}, 0.02, "-=0.2") //0.02 is the time defined between every pixel animation and "-=0.2" is the time when defined for the pixel appear just after $projectImage
         .add("pixelsIn")
-        .fromTo($projectTitle, 0.7, {autoAlpha: 0, xPercent: "-50"}, {autoAlpha: 1, xPercent: "-5", ease:  Power4.easeInOut, x: 0})
-        .fromTo($projectSubtitle, 0.7, {autoAlpha: 0, xPercent: "-50"}, {autoAlpha: 1, xPercent: "-2", ease: Power4.easeInOut})
+        .fromTo($projectTitle, 0.7, {autoAlpha: 0, xPercent: "-50"}, {autoAlpha: 1, xPercent: "-5", ease:  Power4.easeInOut, x: 0}, "-=0.4")
+        .fromTo($projectSubtitle, 0.7, {autoAlpha: 0, xPercent: "-50"}, {autoAlpha: 1, xPercent: "-2", ease: Power4.easeInOut}, "-=0.5")
         .add("titleIn")
-        .to($projectTitle, 2, {xPercent: "+=5", ease: Linear.easeNone})
-        .to($projectSubtitle, 2, {xPercent: "+=2", ease: Linear.easeNone})
+        .to($projectTitle, 4.3, {xPercent: "+=5", ease: Linear.easeNone}, "titleIn-=0.1")     // "titleIn-=0.1" mean this tween happen 0.1 second earlier before .add("titleIn") is defined
+        .to($projectSubtitle, 4.3, {xPercent: "+=2", ease: Linear.easeNone}, "titleIn-=0.2") // "titleIn-=0.1" mean this tween happen 0.1 second earlier before .add("titleIn") is defined
         .add("titleOut")
         .to($projectImage,3 , {xPercent: "+=10", ease: Linear.easeNone})
         .add("imageOut")
         .to($pixels, 2, {x: "-5", ease:Linear.easeNone})
-        .to([$projectTitle, $projectSubtitle], 0.5, {autoAlpha: 0, xPercent: "+=10", ease: Power4.easeInOut}); // manipulate two element with an array
+        .to([$projectTitle, $projectSubtitle], 0.5, {autoAlpha: 0, xPercent: "+=10", ease: Power4.easeInOut}) // manipulate two element with an array
+        .to($projectImage, 0.4, {autoAlpha: 0, xPercent: "+=80", ease: Power4.easeInOut}); // manipulate two element with an array
 })(jQuery);
